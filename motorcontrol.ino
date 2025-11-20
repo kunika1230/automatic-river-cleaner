5f826a2950aa5ffed0023ac745abf3024d92e1c3
kunika1230 <kunikacdac@gmail.com>
2025-11-20 16:37:35 +0530

    motorcontrol.ino

--- motorcontrol.ino
+++ motorcontrol.ino
@@ -1,42 +1,56 @@
 #include <Servo.h>
-Servo s1;
-Servo s2;
-Servo s3;
-Servo s4;
+int Servo1Pos;
+int Servo2Pos;
+int Servo3Pos;
+int Servo4Pos;
 
-void setup()
-{
-  s1.attach(9);
-  s2.attach(10);
-  s3.attach(11);
-  s4.attach(12);
+Servo Servo1;
+Servo Servo2;
+Servo Servo3;
+Servo Servo4;
+
+
+
+
+void setup() {
+  // Attach servos to assigned pins
+  Servo1.attach(9);
+  Servo2.attach(8);
+   Servo3.attach(10);
+   Servo4.attach(11);
 }
 
-void loop()
-{
-  int i,j,k,l;
-  for(i=0, j=180; i<=180, j>=0; i++, j--){
-  s1.write(i);
-  s2.write(j);
-  delay(30);
+ 
+
+void loop() {
+
+    Servo3.write(120); // Rotate servo1 to 90 degrees
+    Servo4.write(120); // Rotate servo1 to 90 degrees
+
+  delay(2000); // Wait for a second to see the movement
+  
+  // Move servo 1 from 0 to 180 degrees
+  for (Servo1Pos=0;Servo1Pos<=180; Servo1Pos ++) {
+    Servo1.write(Servo1Pos);
+    Servo2Pos = 180 -Servo1Pos;
+    Servo2.write(Servo2Pos);
+    delay(50); // Wait for the servo to reach the position
   }
-  delay(3000);
-   for(k=0, l=180; k<=180, l>=0; k++, l--){
-  s3.write(k);
-  s4.write(l);
-  delay(30);
+delay(3000);
+Servo3.write(0); // Return servo1 to 0 degrees
+Servo4.write(0); // Return servo1 to 0 degrees
+
+  delay(2000); // Wait before restarting the loop
+  
+  // Move servo 2 from 180 to 0 degrees
+  for (Servo1Pos= 180;Servo1Pos>= 0; Servo1Pos--) {
+    Servo1.write(Servo1Pos);
+    Servo2.write(Servo2Pos);
+    Servo2Pos= Servo2Pos + 1;
+    delay(50);
   }
-  delay(3000);
-  for(i=180, j=0; i>=0, j<=180; i--, j++){
-  s1.write(i);
-  s2.write(j);
-  delay(30);
-  }
-  delay(1000);
-  for(k=180, l=0; k>=0, l<=180; k--, l++){
-  s3.write(k);
-  s4.write(l);
-  delay(30);
-  }
-  delay(3000);
+delay(3000);
+
+ 
+ 
 }
